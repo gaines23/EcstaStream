54132b495686780ead003a017fbe8771b55ac60d
Melissa <gaines.melissa23@gmail.com>
2022-04-06 22:43:35 -0700

    stuff

--- app/views.py
+++ app/views.py
@@ -213,6 +213,304 @@ def profile(request, id, username):
     )
 
 
+
+
+
+
+
+#@login_required
+#def watchlist_add_movie(request, movieid, media_type=1):
+#    assert isinstance(request, HttpRequest)
+
+#    fav_model = WatchListData.objects.all()
+
+#    if fav_model.filter(Q(user=request.user) & Q(mov_show_id=movieid) & Q(media_type=1)).exists():
+#        fav_model.filter(Q(mov_show_id=movieid) & Q(user=request.user) & Q(media_type=1)).delete()
+#        return HttpResponseRedirect(request.META['HTTP_REFERER']) 
+#    else:
+#        fav_model.create(user=request.user, mov_show_id=movieid, media_type=1)
+#        return HttpResponseRedirect(request.META['HTTP_REFERER']) 
+
+
+#@login_required
+#def watchlist_add_tv(request, tvid, media_type=2):
+#    assert isinstance(request, HttpRequest)
+
+#    fav_model = WatchListData.objects.all()
+
+#    if fav_model.filter(Q(user=request.user) & Q(mov_show_id=tvid) & Q(media_type=2)).exists():
+#        fav_model.filter(Q(mov_show_id=tvid) & Q(user=request.user) & Q(media_type=2)).delete()
+#        return HttpResponseRedirect(request.META['HTTP_REFERER']) 
+#    else:
+#        fav_model.create(user=request.user, mov_show_id=tvid, media_type=2)
+#        return HttpResponseRedirect(request.META['HTTP_REFERER']) 
+ 
+
+#@login_required
+#def watch_list(request):
+#    assert isinstance(request, HttpRequest)
+
+#    watch_list = list(WatchListData.objects.filter(Q(user=request.user)))
+#    watch = list(sorted(watch_list, key = lambda x: x.date_added, reverse=True))
+#    all_watch = WatchListData.objects.all()
+
+#    details = []
+
+#    try:
+#        for x in watch:
+#            id = x.mov_show_id
+#            media = x.media_type
+#            if x.media_type == 1:
+#                details.append([{'movie': movie.details(id)}, movie.watch_providers(id).results['US']])
+#            else:
+#                details.append([{'tv': tv.details(id)}, tv.watch_providers(id).results['US']])
+#    except Exception as e:
+#        pass
+    
+#    context = {'watch':watch,
+#               'watch_list':watch_list,
+#               'details':details,
+#               'all_watch':all_watch,
+#    }
+
+#    return render(request,
+#                  'playlists/watchlist.html',
+#                  context
+#    )
+
+
+
+
+
+
+
+## RATINGS Not Likes 
+
+#@login_required
+#def like(request):
+#    if request.POST.get('action') == 'movie_show':
+#        result = ''
+#        id = int(request.POST.get('pl_movie_show_id'))
+#        playlist = get_object_or_404(Post, id=id)
+
+#        if playlist.likes.filter(id=request.user.id).exists():
+#            playlist.likes.remove(request.user)
+#            playlist.like_count -= 1
+#            result = playlist.like_count
+#            playlist.save()
+#        else:
+#            playlist.likes.add(request.user)
+#            playlist.like_count += 1
+#            result = playlist.like_count
+#            playlist.save()
+
+#        return JsonResponse({'result': result, })
+
+
+
+#Don’t forget to add <span class="pre">.env</span> in your 
+#<span class="pre">.gitignore</span> also, it’s advisable to create a 
+#.env.example with a template of all the variables required for the project.
+
+
+def MovieDetails(request, movieid, media_type=1):
+    assert isinstance(request, HttpRequest)
+
+    details = movie.details(movieid)
+    streaming = movie.watch_providers(movieid)
+    us_streaming = streaming.results['US']
+    credits = details['credits']
+    trailers = details['videos']
+    
+    favorited = FavoriteListData.objects.all()
+    fav = bool
+
+    if favorited.filter(Q(mov_show_id=movieid) & Q(media_type=1)).exists():
+        fav = True
+
+    #watchlist = WatchListData.objects.all()
+    #watch = bool
+
+    #if watchlist.filter(Q(mov_show_id=movieid) & Q(media_type=1)).exists():
+    #    watch = True
+
+    runtime = details.runtime
+    hours = runtime // 60
+    minutes = runtime % 60
+    hours_runtime = "{} hr {} min".format(hours, minutes)
+    
+    imdbid = details.imdb_id
+
+    class ImdbAPI(object):
+        url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
+
+        moviestring = {"i":{imdbid} ,"type":"movie", "r":"json"}
+
+        headers = {
+            'x-rapidapi-host': Imdb_URL,
+            'x-rapidapi-key': URL_API
+        }
+
+        mov_response = requests.request("GET", url, headers=headers, params=moviestring)
+        movie_api = mov_response.json()
+
+    r = ImdbAPI.movie_api
+
+    smlrobj = []
+    for result in details['similar']:
+        smlrobj.append(result)
+
+    mov_seriesID = []
+    seriesid = []
+
+    try:
+        mov_seriesID.append(details.belongs_to_collection['id'])
+        x = series.details(mov_seriesID)
+        seriesid.append(x)
+    except Exception as e:
+        pass
+
+    context = {
+        'details': details,
+        'smlrobj': smlrobj,
+        'imdbid':imdbid,
+        'r':r,
+        'trailers':trailers,
+        'streaming':streaming,
+        'credits':credits,
+        'seriesid':seriesid,
+        'us_streaming':us_streaming,
+        'hours_runtime':hours_runtime,
+        'fav':fav,
+    }
+
+    return render(
+        request,
+        'movies/movie_details.html',
+        context,
+    )
+
+
+def TvDetails(request, tvid, media_type=2):
+    assert isinstance(request, HttpRequest)
+
+    details = tv.details(tvid)
+    tv_title = details.name
+    streaming = tv.watch_providers(tvid)
+    similar = details.similar
+    trailers = details.videos
+    credits = details.credits
+    external_id = details.external_ids
+
+    favorited = FavoriteListData.objects.all()
+    fav = bool
+
+    if favorited.filter(Q(mov_show_id=tvid) & Q(media_type=2)).exists():
+        fav = True
+
+    #watchlist = WatchListData.objects.all()
+    #watch = bool
+
+    #if watchlist.filter(Q(mov_show_id=tvid) & Q(media_type=2)).exists():
+    #    watch = True
+
+    series = []
+
+    try:
+        series.append(details.seasons)
+    except Exception as e:
+        pass
+    
+    imdbid = external_id.imdb_id
+    us_streaming = streaming.results['US']
+
+    class ShowImdbAPI(object):
+        url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
+
+        tvstring = {"i":{imdbid},"type":"series","r":"json"}
+
+        headers = {
+            'x-rapidapi-host': Imdb_URL,
+            'x-rapidapi-key': URL_API
+        }
+
+        tv_response = requests.request("GET", url, headers=headers, params=tvstring)
+        tv_api = tv_response.json()
+
+    r = ShowImdbAPI.tv_api
+
+    context = {
+        'details': details,
+        'imdbid':imdbid,
+        'trailers':trailers,
+        'streaming':streaming,
+        'credits':credits,
+        'us_streaming':us_streaming,
+        'series':series,
+        'r':r,
+        'fav':fav,
+    }
+
+    return render(
+        request,
+        'tvshows/tvdetails.html',
+        context,
+    )
+
+
+def CreditsDetails(request, personid):
+    assert isinstance(request, HttpRequest)
+    
+    details = person.details(personid)
+    credits = details.combined_credits
+    movie_credits = details.movie_credits['cast'] 
+    tv_credits = details.tv_credits['cast']
+
+    cast = credits.cast
+    crew = credits.crew
+    cast_amt = len(cast)
+    crew_amt = len(crew)
+
+    bday = datetime.strptime(details.birthday, '%Y-%m-%d').date().strftime('%B %d, %Y')
+    bday_date = datetime.strptime(details.birthday, '%Y-%m-%d').date()
+    today = date.today()
+
+    one_or_zero = ((today.month, today.day) < (bday_date.month, bday_date.day))
+    year_difference = today.year - bday_date.year
+    age = year_difference - one_or_zero
+
+    knownfor = sorted(cast, key=lambda i:(i['vote_count'],i['vote_average'],i['popularity']), reverse=True)[:8]
+
+    mc_filtered = list(filter(lambda x: (x['release_date'] != ''), movie_credits))
+    mc = sorted(mc_filtered, key = lambda x: x.release_date[:4], reverse=True)
+
+    tv_filtered = list(filter(lambda x: (x['first_air_date'] != ''), tv_credits))
+    tv = sorted(tv_filtered, key = lambda x: x.first_air_date[:4], reverse=True)
+
+
+    context = {
+        'details':details,
+        'credits':credits,
+        'cast':cast,
+        'crew':crew,
+        'bday':bday,
+        'age':age,
+        'castamt':cast_amt,
+        'crewamt':crew_amt,
+        'knownfor':knownfor,
+        'mc':mc,
+        'tv':tv,
+    }
+
+    return render(
+        request,
+        'credits/creditsdetails.html',
+        context,
+    )
+
+
+
+
 @login_required
 def favorite_add_movie(request, movieid, media_type=1):
     assert isinstance(request, HttpRequest)
@@ -273,299 +571,6 @@ def favorites_list(request):
                   context
     )
 
-
-
-
-
-
-
-@login_required
-def watchlist_add_movie(request, movieid, media_type=1):
-    assert isinstance(request, HttpRequest)
-
-    fav_model = WatchListData.objects.all()
-
-    if fav_model.filter(Q(user=request.user) & Q(mov_show_id=movieid) & Q(media_type=1)).exists():
-        fav_model.filter(Q(mov_show_id=movieid) & Q(user=request.user) & Q(media_type=1)).delete()
-        return HttpResponseRedirect(request.META['HTTP_REFERER']) 
-    else:
-        fav_model.create(user=request.user, mov_show_id=movieid, media_type=1)
-        return HttpResponseRedirect(request.META['HTTP_REFERER']) 
-
-
-@login_required
-def watchlist_add_tv(request, tvid, media_type=2):
-    assert isinstance(request, HttpRequest)
-
-    fav_model = WatchListData.objects.all()
-
-    if fav_model.filter(Q(user=request.user) & Q(mov_show_id=tvid) & Q(media_type=2)).exists():
-        fav_model.filter(Q(mov_show_id=tvid) & Q(user=request.user) & Q(media_type=2)).delete()
-        return HttpResponseRedirect(request.META['HTTP_REFERER']) 
-    else:
-        fav_model.create(user=request.user, mov_show_id=tvid, media_type=2)
-        return HttpResponseRedirect(request.META['HTTP_REFERER']) 
- 
-
-@login_required
-def watch_list(request):
-    assert isinstance(request, HttpRequest)
-
-    watch_list = list(WatchListData.objects.filter(Q(user=request.user)))
-    watch = list(sorted(watch_list, key = lambda x: x.date_added, reverse=True))
-    all_watch = WatchListData.objects.all()
-
-    details = []
-
-    try:
-        for x in watch:
-            id = x.mov_show_id
-            media = x.media_type
-            if x.media_type == 1:
-                details.append([{'movie': movie.details(id)}, movie.watch_providers(id).results['US']])
-            else:
-                details.append([{'tv': tv.details(id)}, tv.watch_providers(id).results['US']])
-    except Exception as e:
-        pass
-    
-    context = {'watch':watch,
-               'watch_list':watch_list,
-               'details':details,
-               'all_watch':all_watch,
-    }
-
-    return render(request,
-                  'playlists/watchlist.html',
-                  context
-    )
-
-
-
-
-
-
-
-## RATINGS Not Likes 
-
-#@login_required
-#def like(request):
-#    if request.POST.get('action') == 'movie_show':
-#        result = ''
-#        id = int(request.POST.get('pl_movie_show_id'))
-#        playlist = get_object_or_404(Post, id=id)
-
-#        if playlist.likes.filter(id=request.user.id).exists():
-#            playlist.likes.remove(request.user)
-#            playlist.like_count -= 1
-#            result = playlist.like_count
-#            playlist.save()
-#        else:
-#            playlist.likes.add(request.user)
-#            playlist.like_count += 1
-#            result = playlist.like_count
-#            playlist.save()
-
-#        return JsonResponse({'result': result, })
-
-
-
-#Don’t forget to add <span class="pre">.env</span> in your 
-#<span class="pre">.gitignore</span> also, it’s advisable to create a 
-#.env.example with a template of all the variables required for the project.
-
-
-def MovieDetails(request, movieid, media_type=1):
-    assert isinstance(request, HttpRequest)
-
-    details = movie.details(movieid)
-    streaming = movie.watch_providers(movieid)
-    us_streaming = streaming.results['US']
-    credits = details['credits']
-    trailers = details['videos']
-    
-    favorited = FavoriteListData.objects.all()
-    fav = bool
-
-    if favorited.filter(Q(mov_show_id=movieid) & Q(media_type=1)).exists():
-        fav = True
-
-    watchlist = WatchListData.objects.all()
-    watch = bool
-
-    if watchlist.filter(Q(mov_show_id=movieid) & Q(media_type=1)).exists():
-        watch = True
-
-    runtime = details.runtime
-    hours = runtime // 60
-    minutes = runtime % 60
-    hours_runtime = "{} hr {} min".format(hours, minutes)
-    
-    imdbid = details.imdb_id
-
-    class ImdbAPI(object):
-        url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
-
-        moviestring = {"i":{imdbid} ,"type":"movie", "r":"json"}
-
-        headers = {
-            'x-rapidapi-host': Imdb_URL,
-            'x-rapidapi-key': URL_API
-        }
-
-        mov_response = requests.request("GET", url, headers=headers, params=moviestring)
-        movie_api = mov_response.json()
-
-    r = ImdbAPI.movie_api
-
-    smlrobj = []
-    for result in details['similar']:
-        smlrobj.append(result)
-
-    mov_seriesID = []
-    seriesid = []
-
-    try:
-        mov_seriesID.append(details.belongs_to_collection['id'])
-        x = series.details(mov_seriesID)
-        seriesid.append(x)
-    except Exception as e:
-        pass
-
-    context = {
-        'details': details,
-        'smlrobj': smlrobj,
-        'imdbid':imdbid,
-        'r':r,
-        'trailers':trailers,
-        'streaming':streaming,
-        'credits':credits,
-        'seriesid':seriesid,
-        'us_streaming':us_streaming,
-        'hours_runtime':hours_runtime,
-    }
-
-    return render(
-        request,
-        'movies/movie_details.html',
-        context,
-    )
-
-
-def TvDetails(request, tvid, media_type=2):
-    assert isinstance(request, HttpRequest)
-
-    details = tv.details(tvid)
-    tv_title = details.name
-    streaming = tv.watch_providers(tvid)
-    similar = details.similar
-    trailers = details.videos
-    credits = details.credits
-    external_id = details.external_ids
-
-    favorited = FavoriteListData.objects.all()
-    fav = bool
-
-    if favorited.filter(Q(mov_show_id=tvid) & Q(media_type=2)).exists():
-        fav = True
-
-    watchlist = WatchListData.objects.all()
-    watch = bool
-
-    if watchlist.filter(Q(mov_show_id=movieid) & Q(media_type=1)).exists():
-        watch = True
-
-    series = []
-
-    try:
-        series.append(details.seasons)
-    except Exception as e:
-        pass
-    
-    imdbid = external_id.imdb_id
-    us_streaming = streaming.results['US']
-
-    class ShowImdbAPI(object):
-        url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
-
-        tvstring = {"i":{imdbid},"type":"series","r":"json"}
-
-        headers = {
-            'x-rapidapi-host': Imdb_URL,
-            'x-rapidapi-key': URL_API
-        }
-
-        tv_response = requests.request("GET", url, headers=headers, params=tvstring)
-        tv_api = tv_response.json()
-
-    r = ShowImdbAPI.tv_api
-
-    context = {
-        'details': details,
-        'imdbid':imdbid,
-        'trailers':trailers,
-        'streaming':streaming,
-        'credits':credits,
-        'us_streaming':us_streaming,
-        'series':series,
-        'r':r,
-    }
-
-    return render(
-        request,
-        'tvshows/tvdetails.html',
-        context,
-    )
-
-
-def CreditsDetails(request, personid):
-    assert isinstance(request, HttpRequest)
-    
-    details = person.details(personid)
-    credits = details.combined_credits
-    movie_credits = details.movie_credits['cast'] 
-    tv_credits = details.tv_credits['cast']
-
-    cast = credits.cast
-    crew = credits.crew
-    cast_amt = len(cast)
-    crew_amt = len(crew)
-
-    bday = datetime.strptime(details.birthday, '%Y-%m-%d').date().strftime('%B %d, %Y')
-    bday_date = datetime.strptime(details.birthday, '%Y-%m-%d').date()
-    today = date.today()
-
-    one_or_zero = ((today.month, today.day) < (bday_date.month, bday_date.day))
-    year_difference = today.year - bday_date.year
-    age = year_difference - one_or_zero
-
-    knownfor = sorted(cast, key=lambda i:(i['vote_count'],i['vote_average'],i['popularity']), reverse=True)[:8]
-
-    mc_filtered = list(filter(lambda x: (x['release_date'] != ''), movie_credits))
-    mc = sorted(mc_filtered, key = lambda x: x.release_date[:4], reverse=True)
-
-    tv_filtered = list(filter(lambda x: (x['first_air_date'] != ''), tv_credits))
-    tv = sorted(tv_filtered, key = lambda x: x.first_air_date[:4], reverse=True)
-
-
-    context = {
-        'details':details,
-        'credits':credits,
-        'cast':cast,
-        'crew':crew,
-        'bday':bday,
-        'age':age,
-        'castamt':cast_amt,
-        'crewamt':crew_amt,
-        'knownfor':knownfor,
-        'mc':mc,
-        'tv':tv,
-    }
-
-    return render(
-        request,
-        'credits/creditsdetails.html',
-        context,
-    )
 
 
 
